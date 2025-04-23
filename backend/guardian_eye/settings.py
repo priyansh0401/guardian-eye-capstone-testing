7399cce7091ff175916a6cbef18fe80de37b9c4f
priyansh0401 <priyansh0401@icloud.com>
2025-04-23 15:33:40 +0530

    a

--- backend/guardian_eye/settings.py
+++ backend/guardian_eye/settings.py
@@ -15,8 +15,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-key-for-development-only')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
-
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['.vercel.app']
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
